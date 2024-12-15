95128786e36d9d245a39f069b00cc02d49f68714
Joe Roberts <46022022+Dr-Joe-Roberts@users.noreply.github.com>
2024-12-15 17:55:35 +0000

    Fix treatment randomisation to align with odour source
    Add treatment randomisation alignment with Odour Source and allow users to select the number of treatments (2, 4, or 6).
    
    * Add `selectInput` for the number of treatments.
    * Generate treatment input fields based on the number of treatments selected.
    * Update the `randomise` observer to handle different numbers of treatments.
    * Align treatments with odour sources based on the number of treatments selected.
    * Update the `randomised_positions` table to display the correct number of treatments.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Dr-Joe-Roberts/olfactometer_app?shareId=XXXX-XXXX-XXXX-XXXX).

--- modules/treatment_randomisation_module.R
+++ modules/treatment_randomisation_module.R
@@ -7,17 +7,8 @@ treatmentRandomisationUI <- function(id) {
   tagList(
     h2("Treatment Randomisation", class = "large-text"),
     br(),
-    fluidRow(
-      column(6, textInput(ns("treatment1"), "Treatment 1", value = "")),
-      column(6, textInput(ns("treatment2"), "Treatment 2", value = ""))
-    ),
-    fluidRow(
-      column(6, textInput(ns("treatment3"), "Treatment 3", value = "")),
-      column(6, textInput(ns("treatment4"), "Treatment 4", value = ""))
-    ),
-    fluidRow(
-      column(6, textInput(ns("treatment5"), "Treatment 5", value = ""))
-    ),
+    selectInput(ns("num_treatments"), "Number of Treatments", choices = c(2, 4, 6), selected = 4),
+    uiOutput(ns("treatment_inputs")),
     actionButton(ns("randomise"), "Randomise Treatments", class = "btn btn-randomise"),
     tags$br(), tags$br(),
     h4("Randomised Positions", class = "large-text"),
@@ -34,32 +25,50 @@ treatmentRandomisationServer <- function(id, parent_input) {
       randomised = list()
     )
     
+    # Generate treatment input fields based on the number of treatments selected
+    output$treatment_inputs <- renderUI({
+      ns <- session$ns
+      num_treatments <- as.numeric(input$num_treatments)
+      input_fields <- lapply(1:num_treatments, function(i) {
+        textInput(ns(paste0("treatment", i)), paste("Treatment", i), value = "")
+      })
+      do.call(tagList, input_fields)
+    })
+    
     # Observe randomise button click
     observeEvent(input$randomise, {
       # Get treatments from inputs
-      treatments$original <- list(
-        input$treatment1,
-        input$treatment2,
-        input$treatment3,
-        input$treatment4,
-        input$treatment5
-      )
+      num_treatments <- as.numeric(input$num_treatments)
+      treatments$original <- lapply(1:num_treatments, function(i) {
+        input[[paste0("treatment", i)]]
+      })
       
       # Randomise treatments
       treatments$randomised <- sample(treatments$original)
       
       # Update randomised positions in the data collection tabs
-      updateTextInput(session, "arm1_odour", value = treatments$randomised[1])
-      updateTextInput(session, "arm2_odour", value = treatments$randomised[2])
-      updateTextInput(session, "arm3_odour", value = treatments$randomised[3])
-      updateTextInput(session, "arm4_odour", value = treatments$randomised[4])
-      updateTextInput(session, "arm5_odour", value = treatments$randomised[5])
+      if (num_treatments == 2) {
+        updateTextInput(session, "two_arm_arm1_odour", value = treatments$randomised[1])
+        updateTextInput(session, "two_arm_arm2_odour", value = treatments$randomised[2])
+      } else if (num_treatments == 4) {
+        updateTextInput(session, "arm1_odour", value = treatments$randomised[1])
+        updateTextInput(session, "arm2_odour", value = treatments$randomised[2])
+        updateTextInput(session, "arm3_odour", value = treatments$randomised[3])
+        updateTextInput(session, "arm4_odour", value = treatments$randomised[4])
+      } else if (num_treatments == 6) {
+        updateTextInput(session, "arm1_six_odour", value = treatments$randomised[1])
+        updateTextInput(session, "arm2_six_odour", value = treatments$randomised[2])
+        updateTextInput(session, "arm3_six_odour", value = treatments$randomised[3])
+        updateTextInput(session, "arm4_six_odour", value = treatments$randomised[4])
+        updateTextInput(session, "arm5_six_odour", value = treatments$randomised[5])
+        updateTextInput(session, "arm6_six_odour", value = treatments$randomised[6])
+      }
     })
     
     # Render randomised positions table
     output$randomised_positions <- renderTable({
       data.frame(
-        "Position" = 1:5,
+        "Position" = 1:length(treatments$randomised),
         "Treatment" = treatments$randomised
       )
     })
